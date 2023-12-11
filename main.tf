43f2588f384c65c619238691983df9a04fe2bfd3
DeshnaBera <beradeshna@gmail.com>
2023-12-11 13:53:10 +0530

    added policy

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "test" {
   bucket = "mygh-tf-test-bucket"
+  acl = "public-read"
 
   tags = {
     Name        = "My bucket"
@@ -30,6 +31,7 @@ resource "aws_s3_bucket_object" "object" {
   bucket = aws_s3_bucket.test.id
   key    = "index.html"
   source = "index.html"
+  acl = "public-read"
 
 }
 
@@ -40,4 +42,24 @@ resource "aws_s3_bucket_website_configuration" "example" {
     suffix = "index.html"
   }
 
+}
+
+resource "aws_s3_bucket_policy" "public-acces" {
+    bucket = aws_s3_bucket.test.id
+
+    policy = <<EOF
+    {
+            "Version": "2012-10-17",
+            "Id": "Policy1699337655601",
+            "Statement": [
+            {
+                "Sid": "Stmt1699337635049",
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": "s3:GetObject",
+                "Resource": "arn:aws:s3:::mygh-tf-test-bucket/*"
+            }
+        ]
+    }
+    EOF
 }
